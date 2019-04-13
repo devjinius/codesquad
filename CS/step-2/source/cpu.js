af1681e3e2e38334bc6afe21b30aa7959a69c0b6
eugene94 <eugenekang94@gmail.com>
2019-04-14 00:46:38 +0900

    CS step-2 CPU 구현 주석추가

--- CS/step-2/source/cpu.js
+++ CS/step-2/source/cpu.js
@@ -24,11 +24,13 @@ class CPU {
   execute(IR) {
     let INSTRUCTION, first, second, isVal, third;
 
+    // decode를 통해 유의미한 비트단위로 쪼갬
     [INSTRUCTION, first, second, isVal, third] = this.decode(IR);
 
-    const instWord = excuteList.instList[INSTRUCTION]();
-    const value = excuteList[instWord].call(this, second, third, isVal);
+    const instWord = excuteList.instList[INSTRUCTION](); // 명령어 비트를 string으로 변환
+    const value = excuteList[instWord].call(this, second, third, isVal); // 연산 실행
 
+    // store인경우 메모리에 저장하고 나머지의 경우 dst.reg에 저장
     if (instWord === 'STORE') {
       this.memory.store(value, this.register[transform.bin2dec(first)]);
     } else {
@@ -36,12 +38,12 @@ class CPU {
     }
   }
 
-  // 2진수로 표현된 명령어를 규칙에 따라 해석합니다.
+  // decode를 통해 유의미한 비트단위로 쪼개어 반환합니다.
   decode(IR) {
     const BIT_LENGTH = 16;
-    const binIRArr = transform.dec2bin(IR);
+    const binIRArr = transform.dec2bin(IR); // 10진수의 int를 2진수의 배열의 형태로 만듦
 
-    // 16비트로 맞춰줌
+    // 앞에 0이 있다면 비어 있기 때문에 0을채워 16비트로 맞춰줌
     while (binIRArr.length < BIT_LENGTH) {
       binIRArr.unshift(0);
     }
@@ -50,6 +52,7 @@ class CPU {
     const INSTRUCTION = binIRArr.splice(0, 4).join('');
     let first, second, third, fourth;
 
+    // 조건에 맞추어 유의미한 비트별로 splice 한다.
     if (INSTRUCTION !== '1011') {
       first = binIRArr.splice(0, 3);
       second = binIRArr.splice(0, 3);
--- CS/step-2/source/simulate.js
+++ CS/step-2/source/simulate.js
@@ -5,12 +5,13 @@ const memoryArea = new Array(131071);
 const memory = new Memory(memoryArea);
 const cpu = new CPU(memory);
 
-memory.store(12, 100);
-
 const program = [47114, 47618, 4869, 33892, 38466, 18212];
 
 // 메모리에 프로그램 locate
 memory.locate(program);
+
+// 메모리에 데이터 locate
+memory.store(12, 100);
 
 console.log(`프로그램을 실행하기 전 데이터 : ${memory.load(14)}`);
 
@@ -20,3 +21,6 @@ program.forEach(() => {
 });
 
 console.log(`프로그램을 실행한 후 데이터 : ${memory.load(14)}`);
+
+cpu.reset();
+console.log(cpu.dump());
