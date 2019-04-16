a4b1fc7e6a36a9aa68ebf5ed64698e2b0f5a629c
eugene94 <eugenekang94@gmail.com>
2019-04-16 15:01:40 +0900

    CS step-2 프로그램 데이터 수정

--- CS/step-2/source/simulate.js
+++ CS/step-2/source/simulate.js
@@ -5,25 +5,25 @@ const memoryArea = new Array(131071);
 const memory = new Memory(memoryArea);
 const cpu = new CPU(memory);
 
-const program = [47114, 47618, 4869, 33892, 38466, 18212]; // 예시 프로그램
+const program = [47264, 47618, 4869, 33892, 38466, 18212]; // 예시 프로그램
 const program2 = [45578, 46082, 46598, 6240, 10883, 27909, 24326, 15939]; // AND, OR 조건을 이용한 프로그램
 
 // 메모리에 데이터 locate
-memory.store(12, 100);
+memory.store(162, 100);
 memory.store(10, true);
 memory.store(8, false);
 
 // 메모리에 프로그램 locate
 memory.locate(program);
 
-console.log(`프로그램을 실행하기 전 데이터 : ${memory.load(14)}`);
+console.log(`프로그램을 실행하기 전 데이터 : ${memory.load(164)}`);
 
 program.forEach(() => {
   cpu.execute(cpu.fetch());
   console.log(cpu.dump());
 });
 
-console.log(`프로그램을 실행한 후 데이터 : ${memory.load(14)}`);
+console.log(`프로그램을 실행한 후 데이터 : ${memory.load(164)}`);
 
 console.log('=================================================');
 
